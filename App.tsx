d144ea35e05a76ededb274a715118530ab685861
marceloaraujo28 <marcelo_araujo28@hotmail.com>
2022-09-04 00:38:27 -0300

    feat: create project and add necessary dependencies

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, StatusBar } from "react-native";
+import { Header } from "./src/components/Header";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar
+        backgroundColor="transparent"
+        translucent
+        barStyle="light-content"
+      />
+      <Header />
+      <Home />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
